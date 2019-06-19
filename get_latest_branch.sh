03ca8d6db521a9dd726252ebdeb1a6b6c51069fc
kai <chaoyuemyself@hotmail.com>
2019-06-19 21:26:51 +0800

    将项目在Jenkins的workspace目录的取值提到最外层

--- get_latest_branch.sh
+++ get_latest_branch.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-# $1: jenkins的workspace目录
-# $2: gitlab上的仓库地址
+#$1: 该项目在jenkins的workspace中的目录
+#$2: gitlab上的仓库地址
 
-project=`echo $1|awk -F'[/ .]' '{print $(NF-1)}'`
-[[ -d "$workspace/.git" ]] || git clone $1 $workspace
-cd $1/$project
+#project=`echo $2|awk -F'[/ .]' '{print $(NF-1)}'`
+[[ -d "$1/.git" ]] || git clone $2 $1
+cd $1
 git pull>/dev/null
 git remote update origin --prune &>/dev/null
-for b in $(git branch -r|grep -v HEAD);do
+for b in $(git branch -r|grep -v HEAD);do 
     echo $(git show --pretty=format:'%ct  %an  %s' $b|head -n 1) ${b#origin/}
 done|sort -rn -k 1|awk '{print $NF}'|head -10
-
-
